de68425d355420deed9cb75f9d5f93852cb8d562
yellowxav <88281517+EmpthyLL@users.noreply.github.com>
2025-05-22 18:18:39 +0700

    Update PromptTab.js

--- app/PromptTab.js
+++ app/PromptTab.js
@@ -18,7 +18,7 @@ const schema = yup.object({
   text: yup
     .string()
     .required("Text is required")
-    .min(100, "Text must be more than or equal to 100 characters")
+    .min(100, "Text must be at least to 100 characters")
     .max(1000, "Text can not exceeding 1000 characters"),
 });
 
